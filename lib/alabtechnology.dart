fc29d19df66f6df70d7ba58e26aa7fe13360921b
Saravanan V <saravananv@alabtechnology.com>
2026-06-04 16:09:58 +0530

    fix : added loges .

--- lib/alabtechnology.dart
+++ lib/alabtechnology.dart
@@ -3,3 +3,4 @@ export 'core/theme/font_size.dart';
 export 'core/theme/text_theme.dart';
 export 'features/version/app_version.dart';
 export 'features/handler/failer_and_success_handler.dart';
+export 'features/loges/loges.dart';
